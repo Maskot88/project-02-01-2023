9575dbf3107189f57a62d1d698cc0cfd980dccc8
Lymarenko Dmytro <benimaru046@gmail.com>
2023-01-02 15:45:33 +0100

    Let & const

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+let userName = "Brad";
+// console.log(userName);
+
+let userAge;
+userAge = 59;
+
+const userYearOfBirth = 1963;
+
+// console.log(userName);
+// console.log(userAge);
+
+userAge = 60;
+userName = "Bradly"
+
+const userGender = "male";
+// userGender= 'female';
+
+const AMOUTNT_DAYS_WEEK  = 7;
+
+
+
+let number = 12;
+{
+let number = 89;
+console.log(number);
+}
+console.log(number);
