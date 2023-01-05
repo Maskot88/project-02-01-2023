aa052d8d43ae84e3522eb389edad3f6cf48f79c0
Lymarenko Dmytro <benimaru046@gmail.com>
2023-01-05 10:27:57 +0100

    And HW DIMA

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
+let number = 2;
+let number2 = 4;
+let number3 = 7;
 
-
-let userName = "Brad";
-// console.log(userName);
-
-let userAge;
-userAge = 59;
-
-const userYearOfBirth = 1963;
-
-// console.log(userName);
-// console.log(userAge);
-
-userAge = 60;
-userName = "Bradly"
-
-const userGender = "male";
-// userGender= 'female';
-
-const AMOUTNT_DAYS_WEEK  = 7;
-
-
-
-let number = 12;
-{
-let number = 89;
-console.log(number);
+let result;
+let result2;
+let result3;
+let result4;
+if (number > 0 && number2 > 0) {
+    result = number * number2;
+} else if (number === 10 && number2 === -1 ) {
+    result2 = number / number2;
+} else if (number === 0 || number2 === 0) {
+    result3 = number3;
+} else  ( number === 0 && number2 === 0); {
+    result4 = NaN;
 }
-console.log(number);
+
+console.log(result , result2, result3, result4)
+
+let numberN = 11;
+let res;
+if (numberN > 0 && numberN % 2 === 0 && numberN % 3 === 0 && numberN % 6 === 0){
+    console.log('win');
+} else  {
+    console.log('lose');
+}
+
