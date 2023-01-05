b1f5004e9acfc0bc337264de4cbc7b442a677db7
Lymarenko Dmytro <benimaru046@gmail.com>
2023-01-05 15:53:40 +0100

    And HW

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
+
 let number = 2;
 let number2 = 4;
 let number3 = 7;
 
 let result;
-let result2;
-let result3;
-let result4;
-if (number > 0 && number2 > 0) {
+if ((number > 0 && number2 > 0) || (number < 0 && number2 < 0)) {
     result = number * number2;
-} else if (number === 10 && number2 === -1 ) {
-    result2 = number / number2;
+} else if ((number > 0 && number2 < 0) || (number < 0 && number2 > 0)) {
+    result = number / number2;
+} else if (number === 0 && number2 === 0) {
+    result = NaN;
 } else if (number === 0 || number2 === 0) {
-    result3 = number3;
-} else  ( number === 0 && number2 === 0); {
-    result4 = NaN;
+    result = number3;
 }
 
-console.log(result , result2, result3, result4)
+console.log(result);
 
 let numberN = 11;
-let res;
-if (numberN > 0 && numberN % 2 === 0 && numberN % 3 === 0 && numberN % 6 === 0){
-    console.log('win');
-} else  {
+if (!(numberN > 0 && numberN % 2 === 0 && numberN % 3 === 0 && numberN % 6 === 0)) {
     console.log('lose');
+} else  {
+    console.log('win');
 }
 
+// let numberN = 12;
+// if (numberN > 0 && numberN % 2 === 0 && numberN % 3 === 0 && numberN % 6 === 0) {
+//     console.log('win');
+// } else  {
+//     console.log('lose');
+// }
