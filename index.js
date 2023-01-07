ad1377b335c3043670d792d2d513767e0841878a
Lymarenko Dmytro <benimaru046@gmail.com>
2023-01-07 16:44:54 +0100

    End HW JS

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
+// debugger
 
-let number = 2;
-let number2 = 4;
-let number3 = 7;
+let word = prompt();
+const wordReturn = function (value = 5) {
+  if (value % 2 === 0) {
+    return "even";
+  }
+  return "odd";
+};
+console.log(wordReturn(word));
 
-let result;
-if ((number > 0 && number2 > 0) || (number < 0 && number2 < 0)) {
-    result = number * number2;
-} else if ((number > 0 && number2 < 0) || (number < 0 && number2 > 0)) {
-    result = number / number2;
-} else if (number === 0 && number2 === 0) {
-    result = NaN;
-} else if (number === 0 || number2 === 0) {
-    result = number3;
-}
-
-console.log(result);
-
-let numberN = 11;
-if (!(numberN > 0 && numberN % 2 === 0 && numberN % 3 === 0 && numberN % 6 === 0)) {
-    console.log('lose');
-} else  {
-    console.log('win');
-}
-
-// let numberN = 12;
-// if (numberN > 0 && numberN % 2 === 0 && numberN % 3 === 0 && numberN % 6 === 0) {
-//     console.log('win');
-// } else  {
-//     console.log('lose');
-// }
+// /**
+//  *
+//  * @param {number} value1
+//  * @param {number} value2
+//  * @returns {number | null}
+//  */
+// const returneValue = function (value1 = 55, value2 = 50) {
+//   if (value1 > value2) {
+//     return value1;
+//   }
+//   if (value1 < value2) {
+//     return value2;
+//   }
+//   if (value1 === value2) {
+//     return null;
+//   }
+//   return null;
+// };
+// console.log(returneValue());
