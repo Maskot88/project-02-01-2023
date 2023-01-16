237a2a31bea550625e692b3c0d2f61467519a284
Lymarenko Dmytro <benimaru046@gmail.com>
2023-01-16 14:16:37 +0100

    Used arguments in MeArrayPrototype

--- JS/MyArray.js
+++ JS/MyArray.js
@@ -1,26 +1,44 @@
 //constructor for prototype
-function MyArrayPrototype(){
-    this.push = function(value){
-      this[this.length] = value;
-      return ++this.length;
+function MyArrayPrototype() {
+  this.push = function () {
+    for (let index = 0; index < arguments.length; index++) {
+      this[this.length++] = arguments[index];
+      // this.length++;
     }
+    return this.length;
+  };
+  this.pop = function () {
+    if (this.length === 0) {
+      return;
+    }
+    const lastItem = this[this.length - 1];
+    delete this[--this.length];
+    // this.length--;
+    return lastItem;
+  };
+}
+//constructor with data
+function MyArray() {
+  this.length = 0;
+  for (let index = 0; index < arguments.length; index++) {
+    this.push(arguments[index]);
   }
-  //constructor with data
-  function MyArray() {
-    this.length = 0;
-  }
-  
-  MyArray.prototype = new MyArrayPrototype();
-  
-// const myArrayNumbers = new MyArray();
-// const myArrayNumbers2 = new MyArray();
-// myArrayNumbers.push(777);
-// console.log(myArrayNumbers[0]);
-// console.log(myArrayNumbers);
-// console.log(myArrayNumbers.push === myArrayNumbers2.push);
+}
 
-// const arrayNumbers = [];
-// const arrayNumbers2 = new Array();
-// arrayNumbers.push(555);
-// console.log(arrayNumbers);
-// console.log(arrayNumbers.push === arrayNumbers2.push);
+MyArray.prototype = new MyArrayPrototype();
+// MyArray.prototype.newMethod = superMethod;
+
+const myArrayNumbers = new MyArray(8, 7, 6);
+myArrayNumbers.push(777, 4, 5, 7, 8);
+console.log(myArrayNumbers);
+
+myArrayNumbers.pop();
+myArrayNumbers.pop();
+console.log(myArrayNumbers);
+
+const arrayNumbers = new Array(3, 5, 7);
+arrayNumbers.push(45);
+console.log(arrayNumbers);
+// Array.prototype.newMethod = superMethod;+
+
+// function superMethod(){console.log('new method!!!')};
--- index.js
+++ index.js
@@ -1,33 +1,9 @@
-// Запитувати у користувача число до тих пір, доки воно не буде більше 15 і менше 35 і кратне 6 (18,24,30 підходить)
-// В користувача є 3 спроби
-
-// зробити двома способами через while і for
-
-// * це повинна бути функція(можливо і не одна)
-/**
- *
- * @param {number} userInput
- * @returns {boolean}
- */
-function askUser(userInput) {
-  if (userInput >= 15 && userInput <= 30 && userInput % 6 === 0) {
-    return true;
+function sum() {
+  let summa = 0;
+  for (let index = 0; index < arguments.length; index++) {
+    summa += arguments[index];
   }
-}
-//while
-let i = 0;
-while (i < 3) {
-  const userInput = prompt("Enter value");
-  if (askUser(userInput) === true) {
-    break;
-  }
-  i++;
+  return summa;
 }
 
-//for
-for (let i = 0; i < 3; i++) {
-  const userInput = prompt("Enter value");
-  if (askUser(userInput) === true) {
-    break;
-  }
-}
+console.log(sum(1, 2, 3, 7));
