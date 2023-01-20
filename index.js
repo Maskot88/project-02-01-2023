961ee835875d0c36c4a9aa508036eef06c35f892
Lymarenko Dmytro <benimaru046@gmail.com>
2023-01-20 19:21:05 +0100

    ARRAY

--- index.js
+++ index.js
@@ -1,54 +1,22 @@
 "use strict";
 
-// Cтворіть функцію конструктор Продукт з властивостями:
-// назва(рядок), ціна(число), кількість(число - залишок на складі)
-
-// Cтворіть функцію конструктор прототипу Продукту з функціоналом:
-// showInfo() - повертає рядок з усіма данними
-// setSaleToPrice(value=0) - встановлює знижку і повертає нову ціну
-// buyAmountProduct(amountValue) - повертає загальну вартість, якщо є достатня кількість, якщо не має повертаєте - null
 
 
-function Proto (){
-    this.showInfo = function(){ 
-        return 'name: ' + this.name + ', price: ' + this.price + ', remainder: ' + this.remainder;
-    },
-    /**
-     * 
-     * @param {number} value 
-     * @returns {number}
-     */
-    this.setSaleToPrice = function(value=0){ 
-        return this.price -=value;
-    },
-     /**
-     * 
-     * @param {number} value 
-     * @returns {number}
-     */
-    this.buyAmountProduct = function(amountValue){ 
-        if(amountValue > this.remainder){
-            return null;
-        }
-        return amountValue * this.price;
+
+
+// Написати функцію, яка приймає масив чисел
+// і повертає true, якщо в цьому масиві є два однакових числа поспіль
+// Якщо не має - повертає false, якщо масив порожній - теж повертає false
+// [1,2,3,4,5]->false
+// [1,2,2,3,2]->true
+function check(a) {
+    for (let i=1; i<a.length; ++i) {
+      if (a[i] === a[i-1]) {
+        return true;
+      }
     }
-}
-/**
- * 
- * @param {string} name 
- * @param {number} price 
- * @param {number} remainder 
- */
-function Product (name, price, remainder){
-    this.name = name,
-    this.price = price,
-    this.remainder = remainder
-}
-
-Product.prototype = new Proto();
-const product = new Product("product", 100, 10);
-
-console.log(product);
-console.log(product.showInfo());
-console.log(product.setSaleToPrice(15));
-console.log(product.buyAmountProduct(9));
+    
+    return false;
+  }
+  
+  console.log(check([3, 1, 1, 12]));
